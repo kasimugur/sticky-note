4f142517d93e45ef83ce4200abfb9b99143889c6
kasimugur <kasiugur7@gmail.com>
2024-06-05 23:34:42 +0300

    note sticky position add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
+import { useRef, useState, useEffect } from 'react'
 import './App.css'
+
 
 function App() {
 
+  const screen = useRef(null)
+  const [mode, setMode] = useState(false)
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0
+  })
+
+  useEffect(() => {
+    screen.current.focus()
+  }, [])
+
+  const handleKeyUp = (e) => {
+    if (e.key === 'c')
+      setMode(!mode)
+  }
+  const handleMouseMove = (e) => {
+    setPosition({
+      x: e.pageX,
+      y: e.pageY
+    })
+  }
   return (
-    <>-
-  <div className="ma">test</div>
+    <>
+      <div ref={screen} onMouseMove={handleMouseMove} tabIndex={0} onKeyUp={handleKeyUp} className='screen'>
+
+        <img src="https://content-management-files.canva.com/cdn-cgi/image/f=auto,q=70/1bcedaff-72ea-4cf0-a688-4c8b5a54a87a/websites_how-to_2x.png" width={1600} height={730} alt="" />
+
+<div style={{position: 'fixed', top: position.y, left:position.x}}> yorum yazmak için tıkla</div>
+        {mode && (<div> yorum modu aktif </div>)}
+      </div>
     </>
   )
 }
