54820293869c367e0066a34a742c880ffa00abb6
kasimugur <kasiugur7@gmail.com>
2024-06-05 23:48:41 +0300

    leavecommenttext add and updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useRef, useState, useEffect } from 'react'
 import './App.css'
+import { MainContext } from './context'
+import LeaveCommentText from './components/LeaveCommentText'
 
 
 function App() {
@@ -25,15 +27,20 @@ function App() {
       y: e.pageY
     })
   }
+  const data = {
+    position,
+  }
   return (
     <>
-      <div ref={screen} onMouseMove={handleMouseMove} tabIndex={0} onKeyUp={handleKeyUp} className='screen'>
+      <MainContext.Provider value={data}>
+        <div ref={screen} onMouseMove={handleMouseMove} tabIndex={0} onKeyUp={handleKeyUp} className='screen'>
 
-        <img src="https://content-management-files.canva.com/cdn-cgi/image/f=auto,q=70/1bcedaff-72ea-4cf0-a688-4c8b5a54a87a/websites_how-to_2x.png" width={1600} height={730} alt="" />
+          <img src="https://content-management-files.canva.com/cdn-cgi/image/f=auto,q=70/1bcedaff-72ea-4cf0-a688-4c8b5a54a87a/websites_how-to_2x.png" width={1600} height={730} alt="" />
 
-
-        {mode && (<div> yorum modu aktif </div>)}
-      </div>
+<LeaveCommentText />
+          {mode && (<div> yorum modu aktif </div>)}
+        </div>
+      </MainContext.Provider>
     </>
   )
 }
--- src/components/LeaveCommentText.jsx
+++ src/components/LeaveCommentText.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react"
+import { MainContext } from "../context"
+
 export default function LeaveCommentText() {
+  const { position } = useContext(MainContext)
   return (
     <>
-    <div style={{position: 'fixed', top: position.y, left:position.x}}> yorum yazmak için tıkla</div>
+      <div style={{ position: 'fixed', top: position.y, left: position.x }}> yorum yazmak için tıkla</div>
     </>
   )
 }
