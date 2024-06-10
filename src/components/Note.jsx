03c55490b7eb1611843fbc297e4c0fcf0d46d65f
kasimugur <kasiugur7@gmail.com>
2024-06-10 09:59:44 +0300

    note updated and draggable add

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,18 +1,21 @@
 import { useState } from "react"
+import Draggable from "react-draggable"
 
 export default function Note(note) {
   const [visible, setVisible] = useState(false)
   return (
     <>
-    <div className="note-container" style={{
-      '--color' : note.color,
-      position: 'absolute', top: note.position.y, left: note.position.x
-    }}>
-<span onClick={() => setVisible(!visible)} className="note-box-number">{(note.number + 1) - 1} </span>
-<div className="note" style={{display: visible ? 'flex' : 'none'}}>
-  {note.note}
-</div>
-    </div>
+      <Draggable defaultPosition={{ x: note.position.x, y: note.position.y }}>
+        <div className="note-container" style={{
+          '--color': note.color,
+          position: 'absolute',
+        }}>
+          <span onClick={() => setVisible(!visible)} className="note-box-number">{(note.number + 1) - 1} </span>
+          <div className="note" style={{ display: visible ? 'flex' : 'none' }}>
+            {note.note}
+          </div>
+        </div>
+      </Draggable>
     </>
   )
 }
