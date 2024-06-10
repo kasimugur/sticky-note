db90012daf9fafa21ace6b16d3e419977344fe97
kasimugur <kasiugur7@gmail.com>
2024-06-10 09:38:13 +0300

    note components updated

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react"
+
 export default function Note(note) {
+  const [visible, setVisible] = useState(false)
   return (
     <>
-    <div className="note" style={{
+    <div className="note-container" style={{
       '--color' : note.color,
       position: 'absolute', top: note.position.y, left: note.position.x
     }}>
-
-      
+<span onClick={() => setVisible(!visible)} className="note-box-number">{(note.number + 1) - 1} </span>
+<div className="note" style={{display: visible ? 'flex' : 'none'}}>
+  {note.note}
+</div>
     </div>
     </>
   )
